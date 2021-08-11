a2f7ce8320aa2b25d6b5a7cd1f59859ab4d37c0b
tanguy deleage <tanguy.deleage0@gmail.com>
2021-08-11 16:56:50 +0200

    Franchement j'ai essayé de commencer l'exo mais flemme

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,24 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flats.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+end
